b0b35e917777b947a04b4306f215d2e27e59165f
AutomeEdwin <automeedwin@gmail.com>
2021-06-01 01:10:54 +0200

    signin api route working

--- backend/concretetracker/models.py
+++ backend/concretetracker/models.py
@@ -1,9 +1,9 @@
+"""test"""
 from django.db import models
-
-# Create your models here.
 
 
 class User(models.Model):
+    """test"""
     firstName = models.CharField(max_length=20, blank=False, default='')
     lastName = models.CharField(max_length=20, blank=False, default='')
     email = models.CharField(max_length=50, blank=False, default='')
--- backend/concretetracker/urls.py
+++ backend/concretetracker/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from concretetracker import views
 
 urlpatterns = [
-    url(r'^api/signup', views.signup)
+    url(r'^api/signup', views.signup),
+    url(r'^api/signin', views.signin)
 ]
