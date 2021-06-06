68cf75844f3f361769aafd80dc75fbfe8e8db7d1
AutomeEdwin <automeedwin@gmail.com>
2021-06-06 14:35:07 +0200

    cleaned comments

--- frontend/src/app/services/localstorage.service.spec.ts
+++ frontend/src/app/services/localstorage.service.spec.ts
@@ -16,17 +16,14 @@ describe('LocalstorageService', () => {
   });
 
   it('should store the token in the localstorage', () => {
-    // TODO
     localStorageService.set('token', 'testToken');
     expect(localStorage.getItem('token')).toEqual('testToken');
   });
   it('should get the token from the localstorage', () => {
-    // TODO
     localStorage.setItem('token', 'testToken');
     expect(localStorageService.get('token')).toEqual('testToken');
   });
   it('should remove the token from the localstorage', () => {
-    // TODO
     localStorage.setItem('token', 'testToken');
     localStorageService.remove('token');
     expect(localStorage.getItem('token')).toEqual(null);
