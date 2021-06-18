fe88f96345fa0418834f64574a0a41986f6b46fb
AutomeEdwin <automeedwin@gmail.com>
2021-06-18 21:59:50 +0200

    renamed app

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -16,9 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ConcreteTracker'`, () => {
+  it(`should have as title 'MyConcreteCasting'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ConcreteTracker');
+    expect(app.title).toEqual('MyConcreteCasting');
   });
 });
