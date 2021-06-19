3d3ea7240a70dd5363110f7e0caef6ba07a0723d
AutomeEdwin <automeedwin@gmail.com>
2021-06-19 18:05:12 +0200

    overview step is now displaying informations about the futur jobsite and its castings

--- frontend/src/app/new-jobsite/new-jobsite.component.ts
+++ frontend/src/app/new-jobsite/new-jobsite.component.ts
@@ -6,6 +6,8 @@ import {
   FormControl,
   FormArray,
 } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+
 import Map from 'ol/Map';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 import OSM from 'ol/source/OSM';
@@ -18,6 +20,12 @@ import { MapBrowserEvent } from 'ol';
   selector: 'app-new-jobsite',
   templateUrl: './new-jobsite.component.html',
   styleUrls: ['./new-jobsite.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
 })
 export class NewJobsiteComponent implements OnInit {
   jobsiteForm!: FormGroup;
@@ -26,8 +34,8 @@ export class NewJobsiteComponent implements OnInit {
   concreteCastings!: FormArray;
 
   map!: Map;
-  lattitude: number = 0;
-  longitude: number = 0;
+  lattitude!: number;
+  longitude!: number;
 
   constructor(private formBuilder: FormBuilder) {}
 
@@ -45,10 +53,13 @@ export class NewJobsiteComponent implements OnInit {
     this.jobsiteForm = this.formBuilder.group({
       jobsite_name: new FormControl('', [Validators.required]),
       jobsite_description: new FormControl(''),
-      jobsite_coordinates: new FormControl({
-        lattitude: Number,
-        longitude: Number,
-      }),
+      jobsite_coordinates: new FormControl(
+        {
+          lattitude: Number,
+          longitude: Number,
+        },
+        [Validators.required]
+      ),
     });
   }
 
@@ -56,13 +67,14 @@ export class NewJobsiteComponent implements OnInit {
     return this.jobsiteForm.controls;
   }
 
+  getJobsiteDatas(key: string) {
+    return this.jobsiteForm.get(key)?.value;
+  }
+
   // SECOND FORM FOR CONCRETE CASTING CREATION
   initConcreteCastingsForm(): void {
     this.concreteCastingsForm = this.formBuilder.group({
-      concreteCastings: this.formBuilder.array(
-        [this.createConcreteCasting()],
-        [Validators.required]
-      ),
+      concreteCastings: this.formBuilder.array([this.createConcreteCasting()]),
     });
   }
 
@@ -70,11 +82,22 @@ export class NewJobsiteComponent implements OnInit {
     return <FormArray>this.concreteCastingsForm.get('concreteCastings');
   }
 
+  getCastingsDatas(i: number, key: number) {
+    this.concreteCastings = this.concreteCastingsForm.get(
+      'concreteCastings'
+    ) as FormArray;
+    let val = this.concreteCastings.at(i).value;
+
+    val = Object.keys(val).map((key) => val[key]);
+
+    return val[key];
+  }
+
   createConcreteCasting(): FormGroup {
     return this.formBuilder.group({
-      casting_name: new FormControl(''),
+      casting_name: new FormControl('', [Validators.required]),
       casting_description: new FormControl(''),
-      casting_infos: new FormControl(''),
+      casting_infos: new FormControl('', [Validators.required]),
     });
   }
 
