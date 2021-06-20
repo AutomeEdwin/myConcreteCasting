02694cf7cb034a8e2e3e2ceab3fc7a7565ad36fd
AutomeEdwin <automeedwin@gmail.com>
2021-06-20 18:21:09 +0200

    added logout for backend

--- backend/myconcretecasting/urls.py
+++ backend/myconcretecasting/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/', LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout')
 ]
