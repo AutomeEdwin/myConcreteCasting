89e5df4a0a9643330d0b0eac89ce6681db302531
AutomeEdwin <automeedwin@gmail.com>
2021-06-21 18:17:30 +0200

    tested component

--- frontend/src/app/logout/logout.component.spec.ts
+++ frontend/src/app/logout/logout.component.spec.ts
@@ -1,25 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { LogoutComponent } from './logout.component';
+import { routes } from '../app-routing.module';
 
 describe('LogoutComponent', () => {
   let component: LogoutComponent;
   let fixture: ComponentFixture<LogoutComponent>;
+  let router: Router;
+  let location: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LogoutComponent ]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes(routes),
+      ],
+      declarations: [LogoutComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LogoutComponent);
     component = fixture.componentInstance;
+    router = TestBed.get(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render element', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const button = compiled.querySelector('button');
+
+    expect(button).toBeTruthy();
+  });
+
+  it('should navigate to login', () => {
+    router.navigate(['login']).then(() => {
+      expect(location.path()).toBe('login');
+    });
   });
 });
