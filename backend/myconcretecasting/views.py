b3486b6a3aad0459ab0a618e5eb3d3281a959f3f
AutomeEdwin <automeedwin@gmail.com>
2021-07-09 12:04:22 +0200

    ...

--- backend/myconcretecasting/views.py
+++ backend/myconcretecasting/views.py
@@ -17,24 +17,33 @@ from .models import Jobsite
 
 
 class RegisterAPI(generics.GenericAPIView):
-    serializer_class = RegisterSerializer
+    permission_classes = (permissions.AllowAny,)
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+    def post(self, request):
+        """
+        Check if the user provides the correct credentials 
+        and if the user doesn't already exist.
+
+        Create a new user if credentials are correct and user doesn't exists
+        """
+        serializer = RegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({
-            "success": True,
-            "message": "User created sucessfully",
-            # "user": UserSerializer(user, context=self.get_serializer_context()).data,
-            # "token": AuthToken.objects.create(user)[1]
-        })
+            "status": status.HTTP_201_CREATED,
+            "message": "User created sucessfully"
+        }, status=status.HTTP_201_CREATED)
 
 
 class Login(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format=None):
+        """
+        Check if the user provides the correct credentials.
+        if they are correct a token is generated for the user 
+        and send in the response with his username(email) and user_id
+        """
         data = JSONParser().parse(request)
         user = authenticate(
             username=data['email'], password=data["password"])
