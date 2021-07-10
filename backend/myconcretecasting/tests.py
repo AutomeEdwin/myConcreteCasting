35547b3dee82930d0619ca3b275b52d0f3481b34
AutomeEdwin <automeedwin@gmail.com>
2021-07-10 14:36:03 +0200

    tested user registration

--- backend/myconcretecasting/tests.py
+++ backend/myconcretecasting/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+
+
+class RegisterTest(TestCase):
+    def testRegistration(self):
+        c = Client()
+        response = c.post('/register/',
+                          {
+                              "first_name": "John",
+                              "last_name": "Doe",
+                              "email": "johnDoe@gmail.com",
+                              "password": "123456789"
+                          }
+                          )
+        self.assertEqual(response.status_code, 201)
+
+        response = c.post('/register/',
+                          {
+                              "first_name": "John",
+                              "last_name": "Doe",
+                              "email": "johnDoe.com",
+                          }
+                          )
+        self.assertEqual(response.status_code, 400)
+
