9769158d765835cf4fe19bf8d36a2dae50af14ba
AutomeEdwin <automeedwin@gmail.com>
2021-07-17 13:14:35 +0200

    testing

--- frontend/src/app/jobsite-preview-card/jobsite-preview-card.component.spec.ts
+++ frontend/src/app/jobsite-preview-card/jobsite-preview-card.component.spec.ts
@@ -1,11 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { Jobsite } from '../models/jobsite.model';
+import { Router } from '@angular/router';
 
 import { JobsitePreviewCardComponent } from './jobsite-preview-card.component';
 
 describe('JobsitePreviewCardComponent', () => {
   let component: JobsitePreviewCardComponent;
   let fixture: ComponentFixture<JobsitePreviewCardComponent>;
+  let router: Router;
+
   const jobsite = new Jobsite(
     1,
     'owner',
@@ -18,12 +23,14 @@ describe('JobsitePreviewCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [JobsitePreviewCardComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(JobsitePreviewCardComponent);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     component.jobsite = jobsite;
 
--- frontend/src/app/jobsite-viewer/jobsite-viewer.component.spec.ts
+++ frontend/src/app/jobsite-viewer/jobsite-viewer.component.spec.ts
@@ -1,22 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
+import { routes } from '../app-routing.module';
 import { JobsiteViewerComponent } from './jobsite-viewer.component';
+import { Jobsite } from '../models/jobsite.model';
 
 describe('JobsiteViewerComponent', () => {
   let component: JobsiteViewerComponent;
   let fixture: ComponentFixture<JobsiteViewerComponent>;
+  let router: Router;
+
+  const jobsite: Jobsite = new Jobsite(
+    1,
+    'owner',
+    'name',
+    'address',
+    'coordinates',
+    'description',
+    'castings'
+  );
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes(routes),
+      ],
       declarations: [JobsiteViewerComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(JobsiteViewerComponent);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
+    component.jobsite = jobsite;
     fixture.detectChanges();
   });
 
