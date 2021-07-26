1dd01a1e838e1547102bec1de86d76b35481cf9f
AutomeEdwin <automeedwin@gmail.com>
2021-07-26 12:28:36 +0200

    ...

--- frontend/src/app/account-manager/account-manager.component.ts
+++ frontend/src/app/account-manager/account-manager.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormBuilder,
-  Validators,
-  FormControl,
-  Form,
-} from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
 import { LocalStorageService } from '../services/localstorage.service';
 import { AccountService } from '../services/account.service';
 import { ConfirmJobsiteDeleteComponent } from '../confirm-jobsite-delete/confirm-jobsite-delete.component';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-account-manager',
@@ -19,8 +13,14 @@ import { ConfirmJobsiteDeleteComponent } from '../confirm-jobsite-delete/confirm
   styleUrls: ['./account-manager.component.scss'],
 })
 export class AccountManagerComponent implements OnInit {
+  user = new User(
+    JSON.parse(String(this.localStorageService.get('user'))).id,
+    JSON.parse(String(this.localStorageService.get('user'))).firstName,
+    JSON.parse(String(this.localStorageService.get('user'))).lastName,
+    JSON.parse(String(this.localStorageService.get('user'))).email
+  );
+
   constructor(
-    private formBuilder: FormBuilder,
     private localStorageService: LocalStorageService,
     private accountService: AccountService,
     private router: Router,
